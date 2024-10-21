46a98054c9913fd501d6c2f5b6f95ddc70feeb20
Cristina Ayala <crisstinaayala777@gmail.com>
2024-10-20 21:39:15 -0700

    updated code

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
 // 1. Create to variables, firstCard and secondCard.
 // Set their values to a random number between 2-11
 
-let firstCard = 3
+let firstCard = 10
 let secondCard = 7
 
 // 2. Create a variable sum, and set it to the sum of the two cards
 
-let sum = firstCard + secondCard
+let sum = firstCard + secondCard + 6
 
 if(sum < 21){
     console.log("Do you want to draw a new card?☺️")
