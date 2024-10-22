e4c058134558035228662e8aca318714dfe92e1c
Cristina Ayala <crisstinaayala777@gmail.com>
2024-10-22 15:07:43 -0700

    updating...

--- game.js
+++ game.js
@@ -1,14 +1,15 @@
-// 1. Create to variables, firstCard and secondCard.
-// Set their values to a random number between 2-11
-
 let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard 
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+
 
 function startGame(){
+   sumEl.textContent = "Sum: " + sum 
    if(sum <= 20){
       message = "Do you want to draw a new card?☺️"
    }else if(sum === 21){
@@ -18,18 +19,8 @@ function startGame(){
       message = "Damn you're out 🥹"
       isAlive = false
    }
+   messageEl.textContent = message
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 // CHALLENGES
