33c0a74a6baa0793f54c7abeefb17aa0200b713c
Cristina Ayala <crisstinaayala777@gmail.com>
2024-10-21 20:05:04 -0700

    updating

--- game.js
+++ game.js
@@ -9,9 +9,54 @@ let secondCard = 7
 let sum = firstCard + secondCard + 6
 
 if(sum < 21){
-    console.log("Do you want to draw a new card?☺️")
+   console.log("Do you want to draw a new card?☺️")
 }else if(sum === 21){
-    console.log("BLACKJACK!")
+   console.log("BLACKJACK!")
 }else{
-    console.log("Damn you're out 🥹")
+   console.log("Damn you're out 🥹")
 }
+
+
+
+
+
+
+
+
+
+
+
+// CHALLENGE
+// Check if the person is eligible for a Birthday card from the King! (100)
+
+let age = 101
+
+//if less than 100  -> Not eligible
+// else if exactly 100 -> here's your card
+// else -> not eligible
+
+if(age < 100 || age > 100 ){
+    console.log("Sorry, you're not eligible!")
+}else if(age === 100){
+    console.log("Here's your card!!")
+}
+
+
+
+
+
+// Check if the person is old enough to enter to the nightclub (21)
+// Log a suitable message to the console in both casses
+
+// If less than 21 -> "You can't enter to the club"
+// else ---> "Welcome!"
+
+
+//let age = 21
+
+//if(age < 21){
+  //  console.log("Sorry, you're not old enough to enter to the club")
+//}else{
+   // console.log("Welcome Bishhhh!")
+//}
+
